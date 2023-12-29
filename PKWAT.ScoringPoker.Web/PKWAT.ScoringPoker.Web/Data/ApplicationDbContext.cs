99aeeae168e2cf5cbf9e4884e8f5cb29da247bf0
PawelKarczewski <pawel.karczewski@wealtharc.com>
2023-12-29 01:23:28 +0100

    Add blazor with authentication

---
+++ PKWAT.ScoringPoker.Web/PKWAT.ScoringPoker.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+namespace PKWAT.ScoringPoker.Web.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
