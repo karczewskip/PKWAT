29b5c93d1dad428c6368b00168a2adcc4208a3a4
PawelKarczewski <pawel.karczewski@wealtharc.com>
2024-01-04 19:00:14 +0100

    Create estimation methods from blazor

--- PKWAT.ScoringPoker.Domain/ScoringTask/ValueObjects/Estimation.cs
+++ PKWAT.ScoringPoker.Domain/ScoringTask/ValueObjects/Estimation.cs
@@ -1,29 +1,28 @@
 ﻿namespace PKWAT.ScoringPoker.Domain.ScoringTask.ValueObjects
 {
     using PKWAT.ScoringPoker.Domain.Abstraction;
-    using PKWAT.ScoringPoker.Domain.EstimationMethod.Entities;
     using PKWAT.ScoringPoker.Domain.EstimationMethod.ValueObjects;
     using System.Collections.Generic;
 
     public class Estimation : ValueObject
     {
-        public EstimationMethodKey MethodKey { get; protected set; }
+        public int EstimationMethodId { get; protected set; }
         public EstimationMethodValue Value { get; protected set; }
 
         private Estimation() { }
 
-        public static Estimation Create(EstimationMethodKey methodKey, EstimationMethodValue value)
+        public static Estimation Create(int estimationMethodId, EstimationMethodValue value)
         {
             return new Estimation()
             {
-                MethodKey = methodKey,
+                EstimationMethodId = estimationMethodId,
                 Value = value
             };
         }
 
         protected override IEnumerable<object> GetEqualityComponents()
         {
-            yield return MethodKey;
+            yield return EstimationMethodId;
             yield return Value;
         }
     }
---
+++ PKWAT.ScoringPoker.Server/Data/Migrations/20240104173258_AddEstimationMethods.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PKWAT.ScoringPoker.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEstimationMethods : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EstimationMethods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstimationMethods", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstimationMethodPossibleValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EstimationMethodId = table.Column<int>(type: "int", nullable: false),
+                    EstimationMethodValue = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstimationMethodPossibleValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EstimationMethodPossibleValues_EstimationMethods_EstimationMethodId",
+                        column: x => x.EstimationMethodId,
+                        principalTable: "EstimationMethods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EstimationMethodPossibleValues_EstimationMethodId",
+                table: "EstimationMethodPossibleValues",
+                column: "EstimationMethodId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EstimationMethodPossibleValues");
+
+            migrationBuilder.DropTable(
+                name: "EstimationMethods");
+        }
+    }
+}
